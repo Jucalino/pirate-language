7a0204ecd9ef518e1ff16631ca2ad50da5507407
José Luis <joseluis.bmjnet@gmail.com>
2021-08-07 22:52:01 -0300

    correção botão

--- src/app/tradutor/tradutor.component.ts
+++ src/app/tradutor/tradutor.component.ts
@@ -1,8 +1,6 @@
 import { ApiPirataResponse } from './../pirataTraducaoResponse';
 import { PirateService } from './../pirate.service';
-import {
-  MatSnackBar,
-} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -30,27 +28,27 @@ export class TradutorComponent implements OnInit {
 
   onSubmit() {
     if (this.tradutorForm.valid) {
-      this.progress = true
-      this.apiPirata
-        .traduzir(this.tradutorForm.get('texto')?.value)
-        .subscribe(
-          (dados: ApiPirataResponse) => {
-            this.tradutorForm.get('textoTraduzido')?.setValue(dados.contents.translated);
-            this._snackBar.open('Successfully translated!', 'X', {
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
-              duration: 3000,
-            });
-            this.progress = false;
-          },
-          (error: any) => {
-            this.progress = false;
-            this._snackBar.open('Something is wrong!', 'X', {
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
-            });
-          }
-        );
+      this.progress = true;
+      this.apiPirata.traduzir(this.tradutorForm.get('texto')?.value).subscribe(
+        (dados: ApiPirataResponse) => {
+          this.tradutorForm
+            .get('textoTraduzido')
+            ?.setValue(dados.contents.translated);
+          this._snackBar.open('Successfully translated!', 'X', {
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            duration: 3000,
+          });
+          this.progress = false;
+        },
+        (error: any) => {
+          this.progress = false;
+          this._snackBar.open('Something is wrong!', 'X', {
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          });
+        }
+      );
     } else {
       this._snackBar.open('Form is invalid!', 'X', {
         horizontalPosition: 'right',
